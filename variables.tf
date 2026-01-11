f4eaea0f5035da20c91ab1e8a0b8d9abc29b95be
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:47:11 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "dev_test_windows_virtual_machines" {
+  description = <<EOT
+Map of dev_test_windows_virtual_machines, attributes below
+Required:
+    - lab_name
+    - lab_subnet_name
+    - lab_virtual_network_id
+    - location
+    - name
+    - password
+    - resource_group_name
+    - size
+    - storage_type
+    - username
+    - gallery_image_reference (block):
+        - offer (required)
+        - publisher (required)
+        - sku (required)
+        - version (required)
+Optional:
+    - allow_claim
+    - disallow_public_ip_address
+    - notes
+    - tags
+    - inbound_nat_rule (block):
+        - backend_port (required)
+        - protocol (required)
+EOT
+
+  type = map(object({
+    lab_name                   = string
+    lab_subnet_name            = string
+    lab_virtual_network_id     = string
+    location                   = string
+    name                       = string
+    password                   = string
+    resource_group_name        = string
+    size                       = string
+    storage_type               = string
+    username                   = string
+    allow_claim                = optional(bool, true)
+    disallow_public_ip_address = optional(bool)
+    notes                      = optional(string)
+    tags                       = optional(map(string))
+    gallery_image_reference = object({
+      offer     = string
+      publisher = string
+      sku       = string
+      version   = string
+    })
+    inbound_nat_rule = optional(object({
+      backend_port = number
+      protocol     = string
+    }))
+  }))
+}
+
